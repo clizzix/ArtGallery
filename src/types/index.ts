f867986f52259e44d357bc479ca58fb00256f06d
clizzix <godsilla1993@gmail.com>
2026-02-25 19:08:52 +0100

    improved Typing

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
-import { ArtworkNoteSchema, ArtworkSchema } from '../schemas';
+import {
+    ArtworkNoteSchema,
+    ArtworkSchema,
+    ArtworkResponseSchema,
+} from '../schemas';
 import { z } from 'zod';
 
 export type Artwork = z.infer<typeof ArtworkSchema>;
 
-export type Note = z.infer<typeof ArtworkNoteSchema>;
+export type ArtworkResponse = z.infer<typeof ArtworkResponseSchema>;
+
+export type ArtworkWithNote = z.infer<typeof ArtworkNoteSchema>;
